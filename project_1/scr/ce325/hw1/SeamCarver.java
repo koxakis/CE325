e5ad6b20bcfa770941e9656beb9e524a314101a8
Nikolas Koxenoglou <koxakin@gmail.com>
2016-04-06 20:09:33 +0300

    Tested file stucture/ Remove scopes and returns for implementation

--- project_1/scr/ce325/hw1/SeamCarver.java
+++ project_1/scr/ce325/hw1/SeamCarver.java
@@ -1,14 +1,22 @@
 public class SeamCarver{
 
-	public SeamCarver(java.awt.image.BufferedImage image);
-	public SeamCarver(java.io.File file);
-	public SeamCarver(java.net.URL url);
-	public double energy(int row, int col);  // energy of a pixel
-	public int[] findHorizontalSeam();       // return horizontal seam
-	public int[] findVerticalSeam();         // return vertical seam
-	public void removeHorizontalSeam(int[] seam); // remove the seam
-	public void removeVerticalSeam(int[] seam);   // remove the seam
-	private void scale(int width, int height);    // scale to the optimal
+	public SeamCarver(java.awt.image.BufferedImage image){};
+	public SeamCarver(java.io.File file){};
+	public SeamCarver(java.net.URL url){};
+	public double energy(int row, int col){
+		return 0;
+	};  // energy of a pixel
+	public int[] findHorizontalSeam(){
+		int a[]={0};
+		return a;
+	};       // return horizontal seam
+	public int[] findVerticalSeam(){
+		int a[]={0};
+		return a;
+	};         // return vertical seam
+	public void removeHorizontalSeam(int[] seam){}; // remove the seam
+	public void removeVerticalSeam(int[] seam){};   // remove the seam
+	private void scale(int width, int height){};    // scale to the optimal
 	                                                // dimensions before applying
 	                                                 // SeamCarve algorithm
 
