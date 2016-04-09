2d72544708050f96307f89994fe82d0f23e587ec
Nikolas Koxenoglou <koxakin@gmail.com>
2016-04-10 00:33:31 +0300

    Implemented constractor call and userImage object instanciation

--- project_1/scr/ce325/hw1/SeamCarver.java
+++ project_1/scr/ce325/hw1/SeamCarver.java
@@ -1,11 +1,25 @@
 package ce325.hw1;
 
+import java.io.*;
+import java.net.*;
+
 public class SeamCarver {
 
+	public int[][] image;
+
 	//Constractors for different input methods
-	public SeamCarver(java.awt.image.BufferedImage image){};
-	public SeamCarver(java.io.File file){};
-	public SeamCarver(java.net.URL url){};
+	public SeamCarver(java.awt.image.BufferedImage image) throws IOException{
+
+	};
+	public SeamCarver(java.io.File file) throws IOException{
+
+		System.out.println("Hello from File");
+	};
+
+	public SeamCarver(java.net.URL url) throws IOException{
+
+		System.out.println("Hello from URL");
+	};
 
 	public double energy(int row, int col){
 		return 0;
@@ -24,5 +38,37 @@ public class SeamCarver {
 	                                                // dimensions before applying
 	                                                 // SeamCarve algorithm
 
+	public static void main(String[] args) {
 
+		try{
+
+			java.net.URL userInput = new java.net.URL(args[0]);
+			SeamCarver userImage = new SeamCarver(userInput);
+		}catch(Exception ex){
+			ex.printStackTrace();
+			try{
+				java.io.File userInput = new java.io.File(args[0]);
+				SeamCarver userImage = new SeamCarver(userInput);
+			}catch(Exception ex2){
+				ex2.printStackTrace();
+				System.out.println("No valid input given");
+				//Loop for user input
+			}
+		}
+
+		/*try{
+
+
+			if ( isFile ){
+				SeamCarver usrImage = new SeamCarver(userFileInput);
+			}else{
+				SeamCarver usrImage = new SeamCarver(userURLInput);
+			}
+		}catch(IOException ex){
+
+            System.out.println(ex.toString());
+
+		}*/
+
+	}
 }
