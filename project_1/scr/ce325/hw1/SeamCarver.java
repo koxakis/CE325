a4424f3a15ec65aa96155773529c066ea447d616
Nikolas Koxenoglou <koxakin@gmail.com>
2016-04-10 22:48:03 +0300

    Implemented image inputs for both file/URL to a buffered image

--- project_1/scr/ce325/hw1/SeamCarver.java
+++ project_1/scr/ce325/hw1/SeamCarver.java
@@ -1,7 +1,11 @@
 package ce325.hw1;
 
+import javax.annotation.*;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
 import java.io.*;
 import java.net.*;
+import javax.imageio.ImageIO;
 
 public class SeamCarver {
 
@@ -10,17 +14,39 @@ public class SeamCarver {
 	//Constractors for different input methods
 	public SeamCarver(java.awt.image.BufferedImage image) throws IOException{
 		//Take BufferedImage and create RGB and energy tables
+		BufferedImage newInput = new BufferedImage(image.getWidth(), image.getHeight(), BufferedImage.TYPE_INT_ARGB);
+
+		Graphics2D temp = newInput.createGraphics();
+		temp.drawImage(image, 0, 0, null);
+		temp.dispose();
 	};
 	public SeamCarver(java.io.File file) throws IOException{
 		//Use file io to open the image
 		//Convert File->BufferedImage and call said Constractor with this
-		System.out.println("Hello from File");
+		BufferedImage input = ImageIO.read(file);
+		BufferedImage newInput = new BufferedImage(input.getWidth(), input.getHeight(), BufferedImage.TYPE_INT_ARGB);
+
+		Graphics2D temp = newInput.createGraphics();
+		temp.drawImage(input, 0, 0, null);
+		temp.dispose();
+		System.out.println("Hello from File " + input.getType() + " " + newInput.getType() );
 	};
 
 	public SeamCarver(java.net.URL url) throws IOException{
 		//Download image from site
 		//Convert url->BufferedImage and call said Constractor with this
-		System.out.println("Hello from URL");
+		BufferedImage input = ImageIO.read(url);
+		BufferedImage newInput = new BufferedImage(input.getWidth(), input.getHeight(), BufferedImage.TYPE_INT_ARGB);
+
+		Graphics2D temp = newInput.createGraphics();
+		temp.drawImage(input, 0, 0, null);
+		temp.dispose();
+
+		//Oputputs a buffered image to a file Use for final output
+		/*File outputFile = new File("saved.png");
+		ImageIO.write(newInput, "png", outputFile); */
+
+		System.out.println("Hello from URL " + input.getType() + " " + newInput.getType());
 	};
 
 	public double energy(int row, int col){
@@ -47,7 +73,8 @@ public class SeamCarver {
 			java.net.URL userInput = new java.net.URL(args[0]);
 			SeamCarver userImage = new SeamCarver(userInput);
 		}catch(Exception ex){
-			ex.printStackTrace();
+			//ex.printStackTrace();
+			System.out.println(ex.getMessage());
 			try{
 				java.io.File userInput = new java.io.File(args[0]);
 				SeamCarver userImage = new SeamCarver(userInput);
